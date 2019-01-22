e34405db478d5aed39c7cabd0c52a49b4bfd566a
AT\M.S.Odoevsky <odoevsky.maksim@urfu.ru>
2019-01-22 13:05:22 +0500

    no message

--- js/release.js
+++ js/release.js
@@ -19,6 +19,7 @@ document.addEventListener('play', function(e){
 
 function playThis(el) {
     var track = el.previousElementSibling;
+    track.addEventListener("loadstart", function() { alert('Load Starts!')}, true);
     track.addEventListener("canplay", function() { alert('Can Play!')}, true);
     if (track.duration > 0 && !track.paused) {
         track.pause();
